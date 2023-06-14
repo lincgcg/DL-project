c2c3ba30eba2e57874604c48d7a6151170c2241e
“lincgcg <chunganglin@gmail.com>
2023-06-14 18:26:40 +0800

    CNN-1

--- 1.py
+++ 1.py
@@ -5,6 +5,8 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torch.nn.functional as F
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import wandb
 
 class Net(nn.Module):
     def __init__(self):
@@ -30,6 +32,20 @@ class Net(nn.Module):
         output = self.fc2(x)
         return output
 
+# 初始化wanda
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="DL-project",
+    name = "CNN-MNIST",
+    # track hyperparameters and run metadata
+    config={
+    "Learning Rate": 0.2,
+    "Batch size": 64,
+    "model": "CNN"
+    }
+)
+
+
 # check if CUDA is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -50,7 +66,10 @@ train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=True)
 
 # Training the Model
-for epoch in range(10):
+report_steps = 100
+total_loss = 0
+eopch_num = 10
+for epoch in range(eopch_num):
     for batch_idx, (data, targets) in enumerate(train_loader):
         # Get data to cuda if possible
         data = data.to(device=device)
@@ -59,7 +78,12 @@ for epoch in range(10):
         # forward
         scores = model(data)
         loss = criterion(scores, targets)
-        
+        total_loss += loss.item()
+        if (batch_idx + 1) % report_steps == 0:
+            # logging.info("Epoch id: {}, Training steps: {}, Avg loss: {:.3f}".format(epoch, i + 1, total_loss / report_steps))
+            print("Epoch id: {}, Training steps: {}, Avg loss: {:.3f}".format(epoch, batch_idx + 1, total_loss / report_steps))
+            wandb.log({"loss": total_loss / report_steps})
+            total_loss = 0.0
         # backward
         optimizer.zero_grad()
         loss.backward()
@@ -71,6 +95,8 @@ for epoch in range(10):
 def check_accuracy(loader, model):
     num_correct = 0
     num_samples = 0
+    predicted_labels = []
+    true_labels = []
     model.eval()
     
     with torch.no_grad():
@@ -82,6 +108,23 @@ def check_accuracy(loader, model):
             _, predictions = scores.max(1)
             num_correct += (predictions == y).sum()
             num_samples += predictions.size(0)
+            
+            predicted_labels.extend(predictions.cpu().numpy())
+            true_labels.extend(y.cpu().numpy())
+        
+        accuracy = accuracy_score(true_labels, predicted_labels)
+        precision = precision_score(true_labels, predicted_labels, average='macro')
+        recall = recall_score(true_labels, predicted_labels, average='macro')
+        f1 = f1_score(true_labels, predicted_labels, average='macro')
+        
+        wandb.log({"Accuracy": accuracy})
+        wandb.log({"Precision": precision})
+        wandb.log({"Recall": recall})
+        wandb.log({"'F1 Score":  f1})
+        # print('Precision: ', precision)
+        # print('Recall: ', recall)
+        # print('F1 Score: ', f1)
+        # print("==========================================")
         
         print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}')
     
