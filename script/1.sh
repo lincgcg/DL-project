1981a98577454067508d55f6ac129515dcc56566
“lincgcg <chunganglin@gmail.com>
2023-06-14 21:37:09 +0800

    more exper

--- script/1.sh
+++ script/1.sh
@@ -1,18 +1,30 @@
 
-# learning 0.5
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.5 --name CNN_0.5
+# # learning 0.5
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.5 --name CNN_0.5
 
-# learning 0.1
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.1 --name CNN_0.1
+# # learning 0.1
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.1 --name CNN_0.1
 
-# learning 0.05
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.05 --name CNN_0.05
+# # learning 0.05
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.05 --name CNN_0.05
 
-# learning 0.01
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.01 --name CNN_0.01
+# # learning 0.01
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.01 --name CNN_0.01
 
-# learning 0.005
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.005 --name CNN_0.005
+# # learning 0.005
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.005 --name CNN_0.005
 
-# learning 0.001
-CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.0001 --name CNN_0.0001
+# # learning 0.0001
+# CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.0001 --name CNN_0.0001
+
+# learning 0.00005
+CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.00005 --name CNN_0.00005
+
+# learning 0.00001
+CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.00001 --name CNN_0.00001
+
+# learning 0.000005
+CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.000005 --name CNN_0.000005
+
+# learning 0.000001
+CUDA_VISIBLE_DEVICES=0,1 python3 /root/DL-project/1.py --lr 0.000001 --name CNN_0.000001
